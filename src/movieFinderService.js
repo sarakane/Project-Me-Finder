f6c8d6dc2e344a19aff2f7c1738a747a06dbd7c8
Sara Kane <kane.sarak@gmail.com>
2020-07-06 16:45:43 -0700

    Add class for making API calls for actor search
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- src/movieFinderService.js
+++ src/movieFinderService.js
@@ -15,4 +15,21 @@ export class MovieFinderService {
         return jsonifiedResponse;
       });
   }
+
+  async searchActor(searchQuery) {
+    return fetch(`https://api.themoviedb.org/3/search/person?api_key=${process.env.API_KEY}&query=${searchQuery}&language=en-US`)
+      .then(function(response) {
+        if (response.ok && response.status === 200) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        return jsonifiedResponse;
+      });
+  }
 }
