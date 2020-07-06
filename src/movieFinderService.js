43a0cb0f0b8e10fd797b15089f69c0089d2733e1
Sara Kane <kane.sarak@gmail.com>
2020-07-06 16:27:14 -0700

    Add class for making API calls for movie title search
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

---
+++ src/movieFinderService.js
@@ -0,0 +1,18 @@
+export class MovieFinderService {
+  async searchMovie(searchQuery) {
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchQuery}&language=en-US`)
+      .then(function(response) {
+        if (response.ok && response.status === 200) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        return jsonifiedResponse;
+      });
+  }
+}
