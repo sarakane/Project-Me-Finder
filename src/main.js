2491bca4ee408f04ea48b71c1479ff1c32bdf516
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-07 14:56:31 -0700

    add loop for results, display results with jquery
    co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import './styles.css';
 import $ from 'jquery';
 import { MovieFinderService } from './movieFinderService';
 
+function displayResults(response) {
+  if(response.results.length > 5) {
+    for (let i = 0; i < 5; i++) {
+        $("#title"+i).text(response.results[i].title);
+        $("#result"+i+" .image").append(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
+        $(".result"+i+"-summary").append(`Release date: ${response.results[i].release_date}<br>`);
+        $(".result"+i+"-summary").append(response.results[i].overview);
+    }
+  }
+}
+
 $(document).ready(function(){
   let srch = new MovieFinderService();
 
@@ -13,3 +24,5 @@ $(document).ready(function(){
   })();
 });
 
+
+// <li> Here is your movie: ${movie} The official title is: ${response.results[0].title} ${response.results[0].overview}<br> <img src=https://image.tmdb.org/t/p/w500${response.results[0].poster_path} ></li>`);
