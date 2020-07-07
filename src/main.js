c309e4d991a324dfeb33a99b104636963ec0caa9
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-07 11:15:25 -0700

    add console logs to test API calls

--- src/main.js
+++ src/main.js
@@ -2,5 +2,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+import { MovieFinderService } from './movieFinderService';
 
-$
+$(document).ready(function(){
+  let srch = new MovieFinderService();
+
+  (async() => {
+    const response = await srch.searchGenre("action");
+    const response2 = await srch.searchMovieId(706510);
+    console.log(response);
+    console.log(response2);
+  })();
+});
