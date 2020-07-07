0460a7fa94b9546fef4c233b40b64f8be64c0d29
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-07 16:26:50 -0700

    collect search field results and create people search
    co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,24 +4,71 @@ import './styles.css';
 import $ from 'jquery';
 import { MovieFinderService } from './movieFinderService';
 
-function displayResults(response) {
+
+function displayMovieResults(response) {
   if(response.results.length > 5) {
     for (let i = 0; i < 5; i++) {
-        $("#title"+i).text(response.results[i].title);
-        $("#result"+i+" .image").append(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
-        $(".result"+i+"-summary").append(`Release date: ${response.results[i].release_date}<br>`);
-        $(".result"+i+"-summary").append(response.results[i].overview);
+      $("#title"+i).text(response.results[i].title);
+      $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
+      $(".result"+i+"-summary").html(`Release date: ${response.results[i].release_date}<br>`);
+      $(".result"+i+"-summary").html(response.results[i].overview);
+      $("#result"+i).show();
+
+    }
+  } else {
+    for (let i = 0; i < response.results.length; i++){
+      $("#title"+i).text(response.results[i].title);
+      $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
+      $(".result"+i+"-summary").html(`Release date: ${response.results[i].release_date}<br>`);
+      $(".result"+i+"-summary").html(response.results[i].overview);
+      $("#result"+i).show();
+    }
+  }
+}
+
+function displayPeopleResults(response) {
+  if(response.results[0].known_for.length > 5) {
+    for (let i = 0; i < 5; i++) {
+      $("#title"+i).text(response.results[0].known_for[i].title);
+      $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[0].known_for[i].poster_path} >`);
+      $(".result"+i+"-summary").html(`Release date: ${response.results[0].release_date}<br>`);
+      $(".result"+i+"-summary").html(response.results[0].overview);
+      $("#result"+i).show();
+    }
+  } else {
+    for (let i = 0; i < response.results[0].known_for.length; i++){
+      $("#title"+i).text(response.results[0].known_for[i].title);
+      $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[0].known_for[i].poster_path} >`);
+      $(".result"+i+"-summary").html(`Release date: ${response.results[0].known_for[i].release_date}<br>`);
+      $(".result"+i+"-summary").html(response.results[0].known_for[i].overview);
+      $("#result"+i).show();
     }
   }
 }
 
 $(document).ready(function(){
   let srch = new MovieFinderService();
+  $("#movie-search-form").submit(function (event) {
+    event.preventDefault();
+    let title = $("#movie-search").val();
+    $("#movie-search").val("");
 
-  (async() => {
-    const response = await srch.searchMovie("Avengers");
-    displayResults(response);
-  })();
+    (async() => {
+      const response = await srch.searchMovie(title);
+      displayMovieResults(response);
+    })();
+  });
+
+  $("#people-search-form").submit(function (event) {
+    event.preventDefault();
+    let person = $("#people-search").val();
+    $("#people-search").val("");
+
+    (async() => {
+      const response = await srch.searchPerson(person);
+      displayPeopleResults(response);
+    })();
+  });
 });
 
 
