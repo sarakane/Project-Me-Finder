3957ee8ebae3eaed6c7537031d23dbc93cf3b5f6
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-08 14:04:39 -0700

    Clear previous search results, correct spacing issues.

--- src/main.js
+++ src/main.js
@@ -11,7 +11,9 @@ function displayMovieResults(response) {
       $("#title"+i).text(response.results[i].title);
       if (response.results[i].poster_path){
         $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
-      } 
+      } else {
+        $("#result"+i+" .image").empty();
+      }
       $(".result"+i+"-summary").html(`Release date: ${response.results[i].release_date}<br>`);
       $(".result"+i+"-summary").html(response.results[i].overview);
       $("#result"+i).show();
@@ -22,7 +24,9 @@ function displayMovieResults(response) {
       $("#title"+i).text(response.results[i].title);
       if (response.results[i].poster_path){
         $("#result"+i+" .image").html(`<img src=https://image.tmdb.org/t/p/w500${response.results[i].poster_path} >`);
-      } 
+      } else {
+        $("#result"+i+" .image").empty();
+      }
       $(".result"+i+"-summary").html(`Release date: ${response.results[i].release_date}<br>`);
       $(".result"+i+"-summary").html(response.results[i].overview);
       $("#result"+i).show();
@@ -54,40 +58,41 @@ $(document).ready(function(){
   let srch = new MovieFinderService();
   let response;
 
-    $("#add-to-list0").click(function(event) {
-      event.preventDefault();
-      let listTitle0 = document.getElementById("title0").innerHTML;
-      $(".testOutput").append(` ${listTitle0} <br>`);
-    });
+  $("#add-to-list0").click(function(event) {
+    event.preventDefault();
+    let listTitle0 = document.getElementById("title0").innerHTML;
+    $(".testOutput").append(` ${listTitle0} <br>`);
+  });
 
-    $("#add-to-list1").click(function(event) {
-      event.preventDefault();
-      let listTitle1 = document.getElementById("title1").innerHTML;
-      $(".testOutput").append(` ${listTitle1} <br>`);
-    });
+  $("#add-to-list1").click(function(event) {
+    event.preventDefault();
+    let listTitle1 = document.getElementById("title1").innerHTML;
+    $(".testOutput").append(` ${listTitle1} <br>`);
+  });
 
-    $("#add-to-list2").click(function(event) {
-      event.preventDefault();
-      let listTitle2 = document.getElementById("title2").innerHTML;
-      $(".testOutput").append(` ${listTitle2} <br>`);
-    });
+  $("#add-to-list2").click(function(event) {
+    event.preventDefault();
+    let listTitle2 = document.getElementById("title2").innerHTML;
+    $(".testOutput").append(` ${listTitle2} <br>`);
+  });
 
-    $("#add-to-list3").click(function(event) {
-      event.preventDefault();
-      let listTitle3 = document.getElementById("title3").innerHTML;
-      $(".testOutput").append(` ${listTitle3} <br>`);
-    });
+  $("#add-to-list3").click(function(event) {
+    event.preventDefault();
+    let listTitle3 = document.getElementById("title3").innerHTML;
+    $(".testOutput").append(` ${listTitle3} <br>`);
+  });
 
-    $("#add-to-list4").click(function(event) {
-      event.preventDefault();
-      let listTitle4 = document.getElementById("title4").innerHTML;
-      $(".testOutput").append(` ${listTitle4} <br>`);
-    });
+  $("#add-to-list4").click(function(event) {
+    event.preventDefault();
+    let listTitle4 = document.getElementById("title4").innerHTML;
+    $(".testOutput").append(` ${listTitle4} <br>`);
+  });
 
   $("#movie-search-form").submit(function (event) {
     event.preventDefault();
     let title = $("#movie-search").val();
     $("#movie-search").val("");
+    $("#result0, #result1, #result2, #result3, #result4").hide();
 
     (async() => {
       response = await srch.searchMovie(title);
@@ -99,6 +104,7 @@ $(document).ready(function(){
     event.preventDefault();
     let person = $("#people-search").val();
     $("#people-search").val("");
+    $("#result0, #result1, #result2, #result3, #result4").hide();
 
     (async() => {
       const response = await srch.searchPerson(person);
@@ -111,7 +117,7 @@ $("#button0").click(function(event) {
   event.preventDefault();
   $("input:checkbox[name=id-tags0]:checked").each(function() {
     const checkedtags = $(this).val();
-    $(".result0-summary").append(`<br>${checkedtags} `);
+    $("#display-tag0").append(`<br>${checkedtags} `);
   });
 });
     
@@ -119,7 +125,7 @@ $("#button1").click(function(event) {
   event.preventDefault();
   $("input:checkbox[name=id-tags1]:checked").each(function() {
     const checkedtags = $(this).val();
-    $(".result1-summary").append(`<br>${checkedtags} `);
+    $("#display-tag1").append(`<br>${checkedtags} `);
   });
 });
 
@@ -127,7 +133,7 @@ $("#button2").click(function(event) {
   event.preventDefault();
   $("input:checkbox[name=id-tags2]:checked").each(function() {
     const checkedtags = $(this).val();
-    $(".result2-summary").append(`<br>${checkedtags} `);
+    $("#display-tag2").append(`<br>${checkedtags} `);
   });
 });
 
@@ -135,7 +141,7 @@ $("#button3").click(function(event) {
   event.preventDefault();
   $("input:checkbox[name=id-tags3]:checked").each(function() {
     const checkedtags = $(this).val();
-    $(".result3-summary").append(`<br>${checkedtags} `);
+    $("#display-tag3").append(`<br>${checkedtags} `);
   });
 });
 
@@ -143,7 +149,7 @@ $("#button4").click(function(event) {
   event.preventDefault();
   $("input:checkbox[name=id-tags4]:checked").each(function() {
     const checkedtags = $(this).val();
-    $(".result4-summary").append(`<br>${checkedtags} `);
+    $("#display-tag4").append(`<br>${checkedtags} `);
   });
 });
 
